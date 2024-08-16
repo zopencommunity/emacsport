542ec500848be7fce6e02677bd0b6efed2bfa6b0
Igor Todorovski <itodorov@ca.ibm.com>
2024-08-16 10:24:41 -0400

    Add cicd

---
+++ cicd-stable.groovy
@@ -0,0 +1,22 @@
+
+node('linux')
+{
+  stage ('Poll') {
+                // Poll for local changes
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/emacsport.git']]])
+  }
+
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/emacsport.git'), 
+      string(name: 'PORT_DESCRIPTION', value: 'emacs editor' ),
+      string(name: 'BUILD_LINE', value: 'STABLE')
+    ]
+  }
+}
+
