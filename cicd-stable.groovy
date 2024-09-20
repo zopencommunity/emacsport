403820abda2ce8f784b8f1521235c377fb3f52ef
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:28 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -8,12 +8,12 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/emacsport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/emacsport.git']]])
   }
 
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [
-      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/emacsport.git'), 
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/emacsport.git'), 
       string(name: 'PORT_DESCRIPTION', value: 'emacs editor' ),
       string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "v2r5") 
     ]
